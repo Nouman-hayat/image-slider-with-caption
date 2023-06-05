8f7253425997c7eab72e11b8d1838366c9d2cc4c
Nouman hayat <75477552+Nouman-hayat@users.noreply.github.com>
2023-06-05 14:43:12 +0500

    p1

---
+++ js/script.js
@@ -0,0 +1,41 @@
+let index = 1;
+
+function moveslideto (e) {
+	showSlide((index = e));
+};
+
+function arrowcontrol(e)  {
+	showSlide((index += e));
+};
+
+function showSlide(e) {
+	const images = document.querySelectorAll("img");
+	const indicators = document.querySelectorAll(".slider-indicators span");
+	const contents = document.querySelectorAll(".slider-content h3");
+
+	e > images.length ? (index = 1) : null;
+	e < 1 ? (index = images.length) : null;
+
+	for (let image of images) {
+		image.style.opacity = "0";
+	}
+
+	for (let indicator of indicators) {
+		indicator.style.width = "8px";
+		indicator.style.background = "#ffffffaf";
+	}
+
+	for (let content of contents) {
+		content.style.scale = "0";
+		content.style.opacity = "0";
+		content.style.transitionDelay = ".2s";
+	}
+
+	images[index - 1].style.opacity = "1";
+	indicators[index - 1].style.width = "26px";
+	indicators[index - 1].style.background = "#fff";
+	contents[index - 1].style.scale = "1";
+	contents[index - 1].style.opacity = "1";
+};
+
+showSlide();
